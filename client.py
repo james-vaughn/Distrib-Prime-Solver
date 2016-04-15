1ba38000ad9a59451563b70c781f71f0d2d8cfe3
james-vaughn <james.thomas.vaughn>
2016-04-15 15:59:13 -0400

    Skeleton forming

--- client.py
+++ client.py
@@ -1,4 +1,18 @@
 import socket
 
+def connectToHost(sock):
+	host_info = []
+
+	with open(connection_info) as addr_info:
+		host_info = addr_info.readlines()
+
+	host_ip = host_info[0]
+	host_socket = host_info[1]
+
+	sock.connect( (host_ip,host_socket) )
+
 if __name__ == "__main__":
-	
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	connectToHost(sock)
+
+	print(sock.recv(4096))
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import socket
 
+
+NUM_CONNECTIONS = 5
+
 def possiblePrimeList():
 	n = 1
 	while True:
@@ -10,10 +13,29 @@ def possiblePrimeList():
 
 
 def makeSocket():
-	addr_info = open(connection_info)
+	host_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	
+	host_info = []
+	with open(connection_info) as addr_info:
+		host_info = addr_info.readlines()
+
+	port = host_info[1]
+	try:
+		host_sock.bind( ('',port) )
+		return host_sock
+	except socket.error as msg:
+		print("FUCK: "+msg)
+			
 
 
 if __name__ == "__main__":
-	list = possiblePrimeList()
-	for i in range(10):
-		print(next(list))	
+	host_sock = makeSocket()
+	host_sock.listen(NUM_CONNECTIONS)
+	
+	while True:
+		conn, addr = host_sock.accept()
+		host_sock.sendall("Hello to "+conn+":"+addr)
+	#l = possiblePrimeList()
+
+	#for i in range(10):
+	#	host_sock.send(next(list))	
